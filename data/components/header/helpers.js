dc232c4fc3142a949214e7f683cb7d211ddeab11
SopheaKoy <koysophea09@gmail.com>
2024-11-13 01:55:05 +0700

    update

--- data/components/header/helpers.js
+++
@@ -1,7 +0,0 @@
-function processTemplateString(templateStr, data) {
-    if (templateStr != null) {
-        return templateStr.replace(/\\n/g, '<br>').replace(/\n/g, '<br>');
-    } else {
-        return '';
-    }
-}
---
+++ data/excel/header-footer/helpers.js
@@ -0,0 +1,37 @@
+function getPageNumber(pageIndex, lng) {
+    if (pageIndex == null) {
+        return '';
+    }
+    const arabicToKhmerMap = {
+        '0': '០',
+        '1': '១',
+        '2': '២',
+        '3': '៣',
+        '4': '៤',
+        '5': '៥',
+        '6': '៦',
+        '7': '៧',
+        '8': '៨',
+        '9': '៩'
+    };
+
+    const pageNumber = (pageIndex + 1).toString();
+
+    if (lng === "km") {
+        let khmerPageNumber = '';
+        for (let i = 0; i < pageNumber.length; i++) {
+            khmerPageNumber += arabicToKhmerMap[pageNumber[i]];
+        }
+        return khmerPageNumber;
+    } else {
+        return pageNumber;
+    }
+}
+
+function getTotalPages (pages) {
+    if (!pages) {
+        return ''
+    }
+
+    return pages.length
+}
